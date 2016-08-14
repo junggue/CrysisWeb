55367b1f3e39ea271a5980441291cc0940108036
junggue <junggue.yang@gmail.com>
2016-08-14 16:17:00 -0700

    [FEATURES] Skeleton & AddEmployee component

---
+++ src/components/AdminHome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class EmployeeList extends Component {
+  render(){
+    return(
+      <div>
+        <div>This will be a list element</div>
+        <div>This will be a list element</div>
+        <div>This will be a list element</div>
+      </div>
+    )
+  }
+}
